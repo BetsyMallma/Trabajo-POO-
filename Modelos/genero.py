a643e71f2bc033c3ccf1bb48dd95b203730f7cea
Aldo0704 <aldo.tantavilcar@istpargentina.edu.pe>
2026-07-11 23:25:39 -0500

    Ya avance un poco, agregue el logger

---
+++ Modelos/genero.py
@@ -0,0 +1,8 @@
+class Genero:
+    def __init__(self, nombre, descripcion=None):
+        self.id = None
+        self.nombre = nombre
+        self.descripcion = descripcion
+
+    def __str__(self):
+        return f"[{self.id}] {self.nombre} | {self.descripcion or '-'}"
