eac411ab7be320faae3ddec67f0f1c6cf20421c5
BetsyMallma <betsy.mallmav@istpargentina.edu.pe>
2026-07-15 15:25:29 -0500

    Agrega atributos de instancia a la clase SistemaConfig en el método __new__

--- Backend/Config/sistema_config.py
+++ Backend/Config/sistema_config.py
@@ -4,3 +4,9 @@ class SistemaConfig:
     _inst = None
 
     def __new__(cls):
+        if cls._inst is None:
+            cls._inst = super().__new__(cls)
+            cls._inst.nombre  = "SPS - Sistema de Peliculas y Series"
+            cls._inst.version = "1.0"
+            cls._inst.empresa = "Proyecto Academico SPS"
+            cls._inst.autor   = "Betsy"
