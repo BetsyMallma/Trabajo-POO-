590744c6e91cfbfb209c822ef7eae0e79d1dca26
BetsyMallma <betsy.mallmav@istpargentina.edu.pe>
2026-07-15 15:18:28 -0500

    Implementa el patrón Singleton en la clase Logger

--- Backend/Config/looger.py
+++ Backend/Config/looger.py
@@ -2,3 +2,9 @@ import datetime
 
 class Logger:
     _instancia = None
+
+    def __new__(cls):
+        if cls._instancia is None:
+            cls._instancia = super().__new__(cls)
+            cls._instancia._logs = []
+        return cls._instancia
