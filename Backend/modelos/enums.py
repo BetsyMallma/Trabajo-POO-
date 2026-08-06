d9a0f14c9736a05fc60ff403db9e572efafb3553
Aldo0704 <aldo.tantavilcar@istpargentina.edu.pe>
2026-08-06 16:49:16 -0500

    elimine el archivo enums de modelos porque ahora lo implementamos adentro de el archivo titulo

--- Backend/modelos/enums.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-class TipoContenido(str, enum.Enum):
-    PELICULA = "PELICULA"
-    SERIE = "SERIE"
-
-class EstadoVisu(str, enum.Enum):
-    VISTO = "VISTO"
-    PENDIENTE = "PENDIENTE"
